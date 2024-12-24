7e99190a146621922f9b0a72be950b27fec5f73b
joaovts <joakim.vts@gmail.com>
2024-12-24 14:50:50 -0300

    adjust AppDestination MyCoursesBottomAppBar.kt and update libs e icons

---
+++ app/src/main/java/com/example/mycourses/navigation/AppDestination.kt
@@ -0,0 +1,33 @@
+package com.example.mycourses.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.outlined.Star
+import com.example.mycourses.ui.components.BottomAppBarItem
+
+
+sealed class AppDestination(val route: String) {
+    data object Highlight : AppDestination("initialScreen")
+    data object MyCourses : AppDestination("mycourses")
+    data object Account : AppDestination("account")
+
+}
+
+val bottomAppBarItems = listOf(
+    BottomAppBarItem(
+        label = "Cursos",
+        icon = Icons.Filled.DateRange,
+        destination = AppDestination.Highlight
+    ),
+    BottomAppBarItem(
+        label = "Minha Conta",
+        icon = Icons.Filled.AccountCircle,
+        destination = AppDestination.Account
+    ),
+    BottomAppBarItem(
+        label = "Favoritos",
+        icon = Icons.Outlined.Star,
+        destination = AppDestination.MyCourses
+    ),
+)
