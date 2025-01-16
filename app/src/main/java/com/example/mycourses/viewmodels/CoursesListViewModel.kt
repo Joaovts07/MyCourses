b913a8f2838b244c1ce586d0bacb1c0ce6857b25
joaovts <joakim.vts@gmail.com>
2025-01-16 19:57:32 -0300

    add CoursesListViewModel for use in courses

---
+++ app/src/main/java/com/example/mycourses/viewmodels/CoursesListViewModel.kt
@@ -0,0 +1,69 @@
+package com.example.mycourses.viewmodels
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mycourses.model.entities.Course
+import com.example.mycourses.model.repositories.CourseRepository
+import com.example.mycourses.model.repositories.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CoursesListViewModel @Inject constructor(
+    private val courseRepository: CourseRepository,
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    var courses = mutableStateListOf<Course>()
+        private set
+    var favoriteCourses = mutableStateListOf<Course>()
+        private set
+    var isLoading by mutableStateOf(false)
+        private set
+    var errorMessage by mutableStateOf<String?>(null)
+
+    init {
+        loadCourses()
+    }
+
+    private fun loadCourses() {
+        viewModelScope.launch {
+            try{
+                isLoading = true
+                courses.addAll(courseRepository.getHighlightedCourses())
+            }
+            catch (e: Exception){
+                errorMessage = e.message
+            }
+            finally {
+                isLoading = false
+            }
+        }
+    }
+
+    fun loadFavoriteCourses() {
+        viewModelScope.launch {
+            try {
+                isLoading = true
+                favoriteCourses.clear()
+                val user = userRepository.getCurrentUser()
+                if (user != null) {
+                    favoriteCourses.addAll(courseRepository.getFavoriteCourses(user.favoriteCourses))
+                }
+            } catch (e: Exception) {
+                errorMessage = e.message
+                Log.e("CoursesListViewModel", "Erro ao carregar cursos favoritos", e)
+            } finally {
+                isLoading = false
+            }
+        }
+    }
+
+
+}
