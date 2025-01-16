f5cc591719c6a6b60c8a5adbe25117fb1528b016
joaovts <joakim.vts@gmail.com>
2025-01-16 11:58:41 -0300

    add User Repository

---
+++ app/src/main/java/com/example/mycourses/model/repositories/UserRepository.kt
@@ -0,0 +1,23 @@
+package com.example.mycourses.model.repositories
+
+import com.example.mycourses.model.entities.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+class UserRepository @Inject constructor(
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance(),
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+) {
+    suspend fun getCurrentUser(): User? {
+        val userId = auth.currentUser?.uid ?: return null
+        return getUserById(userId)
+    }
+
+    suspend fun getUserById(userId: String): User? {
+        val document = firestore.collection("users").document(userId).get().await()
+        return document.toObject<User>()
+    }
+}
