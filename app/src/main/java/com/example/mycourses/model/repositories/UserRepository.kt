fe3da40e89a5099ae4f837d623cdd91d3b99e188
joaovts <joakim.vts@gmail.com>
2025-01-16 21:03:05 -0300

    Add EditAccountViewModel and use MVVM

--- app/src/main/java/com/example/mycourses/model/repositories/UserRepository.kt
+++ app/src/main/java/com/example/mycourses/model/repositories/UserRepository.kt
@@ -1,5 +1,6 @@
 package com.example.mycourses.model.repositories
 
+import android.util.Log
 import com.example.mycourses.model.entities.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -19,6 +20,17 @@ class UserRepository @Inject constructor(
     suspend fun getUserById(userId: String): User? {
         val document = firestore.collection("users").document(userId).get().await()
         return document.toObject<User>()
+    }
+
+    suspend fun updateUser(user: User): Boolean {
+        return try {
+            val userId = auth.currentUser?.uid ?: return false
+            firestore.collection("users").document(userId).set(user).await()
+            true
+        } catch (e: Exception) {
+            Log.e("UserRepository", "Erro ao atualizar usuário", e)
+            false
+        }
     }
 
 }
