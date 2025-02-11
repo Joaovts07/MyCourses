8339be072c9eb1a642a0dfc03f1d5057407b73e0
joaovts <joakim.vts@gmail.com>
2025-02-11 18:31:03 -0300

    MC-25: refactor model Comments add a default values for get

--- app/src/main/java/com/example/mycourses/model/entities/Comments.kt
+++ app/src/main/java/com/example/mycourses/model/entities/Comments.kt
@@ -1,11 +1,13 @@
 package com.example.mycourses.model.entities
 
+import kotlinx.serialization.Contextual
 import java.util.Date
 
 data class Comment(
     var id: String? = null,
-    val courseId: String,
-    val userId: String,
-    val text: String,
-    val createdAt: Date
+    val courseId: String = "",
+    val userId: String = "",
+    val text: String = "",
+    @Contextual
+    val createdAt: Date = Date()
 )
