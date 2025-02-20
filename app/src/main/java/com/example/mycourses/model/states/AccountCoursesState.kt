2465ee389733505d7beabce376b15907248388d1
joaovts <joakim.vts@gmail.com>
2025-02-19 22:34:33 -0300

    MC-68: refactor AccountCoursesState

--- app/src/main/java/com/example/mycourses/model/states/AccountCoursesState.kt
+++ app/src/main/java/com/example/mycourses/model/states/AccountCoursesState.kt
@@ -2,12 +2,14 @@ package com.example.mycourses.model.states
 
 import com.example.mycourses.model.entities.Course
 import com.example.mycourses.model.entities.EnrolledCourse
+import com.example.mycourses.model.entities.User
 
 sealed class AccountCoursesState {
     object Loading : AccountCoursesState()
     data class Success(
         val enrolledCourses: List<EnrolledCourse>,
-        val myCourses: List<Course>
+        val myCourses: List<Course>,
+        val user: User
     ) : AccountCoursesState()
     data class Error(val message: String) : AccountCoursesState()
 }
