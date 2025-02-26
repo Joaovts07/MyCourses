5b68a3db84b0135fabe71446234481023fc39979
joaovts <joakim.vts@gmail.com>
2025-02-26 14:48:06 -0300

    MC:66: add create a MyCoursesScaffold for default app bar

--- app/src/main/java/com/example/mycourses/ui/components/Componets.kt
+++ app/src/main/java/com/example/mycourses/ui/components/Componets.kt
@@ -3,6 +3,7 @@ package com.example.mycourses.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -11,16 +12,21 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.mycourses.model.states.DialogState
+import com.example.mycourses.navigation.bottomAppBarItems
 
 @Composable
 fun LoadingScreen(modifier: Modifier = Modifier) {
@@ -97,5 +103,28 @@ fun DialogHandler(dialogState: DialogState, onDismiss: () -> Unit) {
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyCoursesScaffold(
+    navController: NavController,
+    selectedItem: BottomAppBarItem,
+    content: @Composable (PaddingValues) -> Unit
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("My Courses") }
+            )
+        },
+        bottomBar = {
+            MyCoursesBottomAppBar(
+                item = selectedItem,
+                items = bottomAppBarItems,
+                onItemChange = { navController.navigate(it.destination.route) }
+            )
+        }
+    ) { paddingValues ->
+        content(paddingValues)
+    }
+}
 
